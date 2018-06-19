b6b6778f071bd3f26e298aecbfa39e34457b9480
HcwXd <e04dl47878@gmail.com>
2018-06-19 13:35:38 +0800

    Get articles content and extract comments from the post

--- get_data.py
+++ get_data.py
@@ -2,9 +2,8 @@ import requests
 import time
 from bs4 import BeautifulSoup
 
-PAGES = 20
+PAGES = 5
 BOARD_NAME = "NBA"
-CONDITION = 50
 
 NOT_EXIST = BeautifulSoup('<a>本文已被刪除</a>', 'lxml').a
 
@@ -19,8 +18,12 @@ previous_index = int(previous_page.split('index')[1].split('.')[0])
 articles = soup.find_all('div', 'r-ent')
 
 index = previous_index+1
-break_point = False
 is_first_page = True
+
+
+titles = []
+contents = []
+comments = []
 
 while(True):
     time.sleep(0.01)
@@ -28,7 +31,6 @@ while(True):
     response = requests.get(url)
     soup = BeautifulSoup(response.text, 'lxml')
     articles = soup.find_all('div', 'r-ent')
-    print(index)
 
     for article in articles:
         meta = article.find('div', 'title').find('a')
@@ -37,22 +39,23 @@ while(True):
         else:
             title = meta.getText().strip()
             link = meta.get('href')
-            push = article.find('div', 'nrec').getText()
             date = article.find('div', 'date').getText()
-            author = article.find('div', 'author').getText()
-            if push == "" or push[0] == "X":
-                push = 0
-            if push == "爆":
-                push = 9999
             split_date = date.split('/')
+
             if not(is_first_page):
                 if(int(split_date[0]) < 6):
-                    break_point = True
                     break
                 if(int(split_date[0]) == 6 and int(split_date[1]) < 12):
-                    break_point = True
                     break
+            response = requests.get(
+                "https://www.ptt.cc"+link)
+            soup = BeautifulSoup(response.text, 'lxml')
+            post = soup.find('div', 'bbs-screen').getText()
+            print(link)
+            post_info = post.split("※ 文章網址: https://www.ptt.cc"+link)
+            if len(post_info) == 2:
+                titles.append(title)
+                contents.append(post_info[0])
+                comments.append(post_info[1])
     index -= 1
     is_first_page = False
-    if(break_point):
-        break
